6a7b47fde8653d8b9c9421fae9499d8f8dd8950c
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-24 01:40:09 +0200

    Comments Service Creation

--- A-Mini-Microservice-App/blog-boilerplate/comments/index.js
+++ A-Mini-Microservice-App/blog-boilerplate/comments/index.js
@@ -8,14 +8,14 @@ app.use(bodyParser.json());
 const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
-    res.send(commentsByPostId[req.params.id] | []);
+    res.send(commentsByPostId[req.params.id] || []);
 });
 
 app.post('/posts/:id/comments', (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const {content} = req.body;
 
-    const comments = commentsByPostId[req.params.id] | [];
+    const comments = commentsByPostId[req.params.id] || [];
     comments.push({id: commentId, content});
 
     commentsByPostId[req.params.id] = comments;
