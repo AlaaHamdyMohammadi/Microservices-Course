6e1657a5ef018f4cfbd956d6f4abf7aff0251e32
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-24 17:07:47 +0200

    Building Post

--- A-Mini-Microservice-App/blog-boilerplate/client/src/App.js
+++ A-Mini-Microservice-App/blog-boilerplate/client/src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
+import PostCreate from "./components/PostCreate";
 
 function App() {
-  return <div className="App">Blog App</div>;
+  return <div className="App container">Blog App
+  
+    <PostCreate/>
+  </div>;
 }
 
 export default App;
---
+++ A-Mini-Microservice-App/blog-boilerplate/client/src/components/PostCreate.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import axios from 'axios';
+
+
+function PostCreate() {
+  const [title, setTitle] = useState("");
+
+  async function handleSubmit(e){
+    e.preventDefault();
+    const res = await axios.post(`http://localhost:4000/posts`, {
+        title
+    });
+
+    setTitle('');
+
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default PostCreate;
