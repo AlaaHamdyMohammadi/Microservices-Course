b627982990a4b42da7e3c582c37faff75d668a11
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-24 18:50:59 +0200

    Fetching and rendering posts

--- A-Mini-Microservice-App/blog-boilerplate/client/src/App.js
+++ A-Mini-Microservice-App/blog-boilerplate/client/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import PostCreate from "./components/PostCreate";
+import PostList from "./components/PostList";
 
 function App() {
-  return <div className="App container">Blog App
-  
-    <PostCreate/>
-  </div>;
+  return (
+    <div className="container">
+      <h1>Blog App</h1>
+      <h3>Create Post :</h3>
+      <PostCreate />
+      <hr />
+      <h3>Posts :</h3>
+      <PostList />
+    </div>
+  );
 }
 
 export default App;
---
+++ A-Mini-Microservice-App/blog-boilerplate/client/src/components/PostList.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function PostList() {
+  const [posts, setPosts] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await axios.get(`http://localhost:4000/posts`);
+      console.log(res.data);
+      setPosts(res.data);
+    }
+    fetchData();
+  }, []);
+
+  const renderedPosts = Object.values(posts).map((post) => {
+    return (
+      <div
+        className="card"
+        style={{ width: "30%", marginButtom: "20px" }}
+        key={post.id}
+      >
+        <div className="card-body">
+          <h5>{post.title}</h5>
+        </div>
+      </div>
+    );
+  });
+
+  return <div className="d-flex justify-content-between flex-wrap flex-row">{renderedPosts}</div>;
+}
+
+export default PostList;
