2f84ef5a708ee42f63bb88c010a17993a473785d
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-25 03:57:03 +0200

    Update Query Service

--- A-Mini-Microservice-App/blog-boilerplate/query/index.js
+++ A-Mini-Microservice-App/blog-boilerplate/query/index.js
@@ -3,34 +3,46 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const axios = require("axios");
 
-
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const posts ={};
+const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-})
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
-app.post('/events', (req, res) => {
-  const {type, data} = req.body;
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
 
-  if(type === "PostCreated"){
-    const {id, title} = data;
-    
-    posts[id] = {id, title, comments: []};
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
   }
   if (type === "CommentCreated") {
-    const {id, content, postId, status} = data;
+    const { id, content, postId, status } = data;
 
     const post = posts[postId];
-    post.comments.push({id, content, status});
+    post.comments.push({ id, content, status });
+  }
+
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[id];
+    const comment = post.comments.find(comment => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+
   }
 
   res.send({});
-})
+});
 
 const PORT = 4002;
 app.listen(PORT, () => {
